bd0a66aaf0f61201772f8509792a29e3c19edb5b
Denis Motruk <denmotruk@gmail.com>
2019-05-18 14:33:05 -0400

    New venv.

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\tommy\PycharmProjects\BlockVote\venv"
+$env:VIRTUAL_ENV="C:\Users\denmo\code\BlockVote\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
