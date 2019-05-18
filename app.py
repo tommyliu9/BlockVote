dd2fdcca4168a358c364dc0c5622e38d61ef015c
Thomas Liu <tommyliu9@gmail.com>
2019-05-18 16:18:09 -0400

    nigerain bbq

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-from flask import Flask, render_template
-
+from flask import Flask, request, render_template
+import pyrebase
 app = Flask(__name__)
+firebaseConfig = {
+    "apiKey": "AIzaSyCEJUbIKPdjh9817ydlSpQlwoTKGFlxwz0",
+    "authDomain": "blockvote-e7945.firebaseapp.com",
+    "databaseURL": "https://blockvote-e7945.firebaseio.com",
+    "projectId": "blockvote-e7945",
+    "storageBucket": "blockvote-e7945.appspot.com",
+    "messagingSenderId": "70448506085",
+    "appId": "1:70448506085:web:a8dac621838a33e4"
+  }
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=["GET","POST"])
 def register():
-    return render_template('register.html')
+
+    if request.method == "POST":
+        username = request.form['email']
+        password = request.form['password']
+        try:
+            auth.create_user_with_email_and_password(username,password)
+            return render_template("login successful")
+        except:
+            return "registration failed"
+
+    else:
+        return render_template('register.html')
 
 @app.route('/create')
 def create():
@@ -19,8 +41,16 @@ def create():
 def vote():
     return render_template('vote.html')
 
-@app.route('/login')
+@app.route('/login', methods=["GET","POST"])
 def login():
+    try:
+        if request.method == "POST":
+            username = request.form['email']
+            password = request.form['password']
+            user = auth.sign_in_with_email_and_password(username,password)
+            auth.get_account_info(user['idToken'])
+    except:
+        print("login failed")
     return render_template('login.html')
 
 
