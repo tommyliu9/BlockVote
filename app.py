fcdbc30e944f5e956716e898e05f0afd47abe929
Kuba Wernerowski <kubawernerowski@gmail.com>
2019-05-18 15:02:07 -0400

    Fixed flask issue with rendering

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/vote')
+def vote():
+    return render_template('vote.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
